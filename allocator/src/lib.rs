9de082dd44bffb5cd3461dc94d22166d9b2d1121
KuangjuX <qcx@tju.edu.cn>
2021-03-10 23:41:38 -0800

    first commit

--- allocator/src/lib.rs
+++ allocator/src/lib.rs
@@ -1,11 +1,11 @@
-#![no_std]
-pub mod freelist;
-
-extern crate alloc;
-
 #![feature(alloc_error_handler)]
+#![no_std]
 
 #[alloc_error_handler]
 fn alloc_error_handler(layout: alloc::alloc::Layout) -> !{
-    panic("allocation error: {:?}", layout)
+    panic!("allocation error: {:?}", layout)
 }
+
+pub mod freelist;
+
+extern crate alloc;
